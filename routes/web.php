1daae0ae5be53e5913cda27ed0bdbb9bd97b1e71
Stefano <strozzostefano@gmail.com>
2025-01-20 13:12:05 +0100

    Added shop page to list all the products in db

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,13 @@ use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\ShopController;
 use App\Http\Middleware\AuthAdmin;
 
 Auth::routes();
 
 Route::get('/', [HomeController::class, 'index'])->name('home.index');
+Route::get('/shop', [ShopController::class, 'index'])->name('shop.index');
 
 
 Route::middleware(['auth'])->group(function(){
