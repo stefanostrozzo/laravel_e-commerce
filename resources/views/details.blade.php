2bf799c0212eb65229dc98c39d4c73a319c69867
Stefano <strozzostefano@gmail.com>
2025-01-22 22:44:34 +0100

    Fixed add to cart and go to cart referrence in shop pages

--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -403,9 +403,18 @@
                             <img loading="lazy" src="{{asset('uploads/products/thumbnails')}}/{{$gimg}}" width="330" height="400" alt="{{$rproduct->name}}" class="pc__img pc__img-second">
                         @endforeach
                     </a>
-                    <button
-                        class="pc__atc btn anim_appear-bottom btn position-absolute border-0 text-uppercase fw-medium js-add-cart js-open-aside"
-                        data-aside="cartDrawer" title="Add To Cart">Add To Cart</button>
+                    @if (Cart::instance('cart')->content()->where('id', $rproduct->id)->count() > 0)
+                      <a href="{{route('cart.index')}}" class="pc__atc btn anim_appear-bottom btn position-absolute border-0 text-uppercase fw-medium btn btn-warning mb-3">Go to Cart</a>
+                    @else
+                      <form name="addtocart-form" method="post" action="{{route('cart.add')}}">
+                        @csrf
+                        <input type="hidden" name="id" value="{{$rproduct->id}}">
+                        <input type="hidden" name="quantity" value="1">
+                        <input type="hidden" name="name" value="{{$rproduct->name}}">
+                        <input type="hidden" name="price" value="{{$rproduct->sale_price == '' ? $rproduct->regular_price : $rproduct->sale_price}}">
+                        <button type="submit" class="pc__atc btn anim_appear-bottom btn position-absolute border-0 text-uppercase fw-medium js-add-cart js-open-aside" data-aside="cartDrawer" title="Add To Cart">Add To Cart</button>
+                      </form>
+                    @endif
                     </div>
     
                     <div class="pc__info position-relative">
